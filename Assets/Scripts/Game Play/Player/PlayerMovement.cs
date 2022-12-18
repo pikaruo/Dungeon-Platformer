7b10435c79dbbfcf4afb79a469e1f34af9445b48
Punto Putra Ardawan <punto.putra99@gmail.com>
2022-12-18 20:00:54 +0700

    Fungsi dash

--- Assets/Scripts/Game Play/Player/PlayerMovement.cs
+++ Assets/Scripts/Game Play/Player/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    [Header("Movement Variable")]
     [SerializeField] float speedPlayer;
     [SerializeField] float jumpPlayer;
 
@@ -18,12 +20,23 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private BoxCollider2D coll;
 
+    [Header("Dash Variable")]
+    [SerializeField] float dashVelocity = 14f;
+    [SerializeField] float dashTime = 0.5f;
+
+    private Vector2 dashingDir;
+    private bool isDashing;
+    private bool canDash = true;
+
+    private TrailRenderer trailRenderer;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         coll = GetComponent<BoxCollider2D>();
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+        trailRenderer = GetComponent<TrailRenderer>();
     }
 
     private void Update()
@@ -33,6 +46,8 @@ public class PlayerMovement : MonoBehaviour
         JumpPlayer();
 
         UpdateAnimateState();
+
+        PlayerDash();
     }
 
     // mengatur pergantian animasi
@@ -88,6 +103,42 @@ public class PlayerMovement : MonoBehaviour
     private bool IsGrounded()
     {
         return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f, jumpableGround);
+    }
+
+    // fungsi player dash
+    private void PlayerDash()
+    {
+        if (Input.GetButtonDown("Dash") && canDash)
+        {
+            isDashing = true;
+            canDash = false;
+            trailRenderer.emitting = true;
+            dashingDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            if (dashingDir == Vector2.zero)
+            {
+                dashingDir = new Vector2(transform.localScale.x, 0);
+            }
+            StartCoroutine(StopDash());
+        }
+
+        if (isDashing)
+        {
+            rb.velocity = dashingDir.normalized * dashVelocity;
+            return;
+        }
+
+        if (IsGrounded())
+        {
+            canDash = true;
+        }
+    }
+
+    // coroutine dash
+    private IEnumerator StopDash()
+    {
+        yield return new WaitForSeconds(dashTime);
+        trailRenderer.emitting = false;
+        isDashing = false;
     }
 
 }
